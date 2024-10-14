e9be45bcd72981b1a9c643dbc3388d865c9cc56a
nhonoree <honoreniyirema@gmail.com>
2024-10-14 14:27:49 -0700

    updates

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Example task 2 content
+def task_2_function():
+    return "Task 2"
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Example task 3 content
+def task_3_function():
+    return "Task 3"
