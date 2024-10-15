d503c9960a1fddfc7837e15cb2cd055b3f6dfb66
nhonoree <honoreniyirema@gmail.com>
2024-10-15 15:38:41 -0700

    updates

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""defining read_file function"""
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end="")
+    """reads filename with utf-8"""
+    with open(fielname, eencoding='utf-8') as f:
+        print(f.read(), end="")
