fbe528300b718374c06b3ebd1174707832381057
nhonoree <honoreniyirema@gmail.com>
2024-10-25 14:47:48 -0700

    updates

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -1,25 +1,37 @@
 import unittest
 from models.square import Square
+from models.rectangle import Rectangle
 
 class TestSquare(unittest.TestCase):
-    def test_square_creation(self):
-        s = Square(5)
-        self.assertEqual(s.size, 5)
+    """Test cases for the Square class."""
+
+    def setUp(self):
+        """Set up a new Square instance for testing."""
+        self.square1 = Square(5)
+        self.square2 = Square(10, 2, 3, 1)
 
     def test_area(self):
-        s = Square(5)
-        self.assertEqual(s.area, 25)
+        """Test the area calculation."""
+        self.assertEqual(self.square1.area(), 25)
+        self.assertEqual(self.square2.area(), 100)
+
+    def test_position(self):
+        """Test the position of the square."""
+        self.assertEqual(self.square2.x, 2)
+        self.assertEqual(self.square2.y, 3)
 
     def test_str_method(self):
-        s = Square(5, 2, 3)
-        self.assertEqual(str(s), "[Square] (1) 2/3 - 5")
+        """Test the __str__ method."""
+        self.assertEqual(str(self.square1), "[Square] (1) 0/0 - 5")
+        self.assertEqual(str(self.square2), "[Square] (1) 2/3 - 10")
 
-    def test_invalid_size(self):
-        with self.assertRaises(TypeError):
-            Square("5")
-        with self.assertRaises(ValueError):
-            Square(0)
-
+    def test_update(self):
+        """Test the update method."""
+        self.square1.update(2, 7, 1, 2)
+        self.assertEqual(self.square1.id, 2)
+        self.assertEqual(self.square1.size, 7)
+        self.assertEqual(self.square1.x, 1)
+        self.assertEqual(self.square1.y, 2)
 
 if __name__ == "__main__":
     unittest.main()
