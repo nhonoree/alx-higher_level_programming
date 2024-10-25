e7f74e8a865abdff7bbdd3b6a7892219cfb4f7c0
nhonoree <honoreniyirema@gmail.com>
2024-10-25 04:54:45 -0700

    updates

--- 0x0C-python-almost_a_circle/rectangle.py
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -1,65 +1,48 @@
-# models/rectangle.py
+class Rectangle:
+    _id_counter = 1  # Class variable to keep track of IDs
 
-from models.base import Base
-
-class Rectangle(Base):
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize the Rectangle class"""
-        super().__init__(id)
+    def __init__(self, width, height, id=None):
+        if id is None:
+            self.id = Rectangle._id_counter
+            Rectangle._id_counter += 1
+        else:
+            self.id = id
         self.width = width
         self.height = height
-        self.x = x
-        self.y = y
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.__width = value
+        self._width = value
 
     @property
     def height(self):
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
-        self.__height = value
+        self._height = value
 
     def area(self):
-        """Return the area of the rectangle"""
         return self.width * self.height
 
+    def display(self):
+        for _ in range(self.height):
+            print('#' * self.width)
+
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.width}/{self.height}"  # Fixed f-string
+
     def to_dictionary(self):
-        """Return dictionary representation of Rectangle"""
-        return {
-            'id': self.id,
-            'width': self.width,
-            'height': self.height,
-            'x': self.x,
-            'y': self.y
-        }
-
-    def update(self, *args, **kwargs):
-        """Update the Rectangle attributes"""
-        attributes = ["id", "width", "height", "x", "y"]
-        if args:
-            for index, value in enumerate(args):
-                if index < len(attributes):
-                    setattr(self, attributes[index], value)
-        elif kwargs:
-            for key, value in kwargs.items():
-                if key in attributes:
-                    setattr(self, key, value)
-
-    # Additional methods like display, __str__, etc.
-
+        return {'id': self.id, 'width': self.width, 'height': self.height}
