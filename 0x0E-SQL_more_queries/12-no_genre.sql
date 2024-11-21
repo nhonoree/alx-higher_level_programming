4edf91431d65ca7cdc73a6de4ef0366de8a3b79a
nhonoree <honoreniyirema@gmail.com>
2024-11-21 14:46:55 -0800

    updates

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,10 +1,14 @@
--- Title: Shows Without a Genre
--- Script to list all shows without a genre linked from `hbtn_0d_tvshows`
--- Each record displays: tv_shows.title
--- Results are sorted in ascending order by tv_shows.title
+-- Script to list all shows that do not have a genre linked.
+-- Results are sorted by tv_shows.title and tv_show_genres.genre_id in ascending order.
 
-SELECT tv_shows.title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title;
+SELECT 
+    tv_shows.title, 
+    tv_show_genres.genre_id
+FROM 
+    tv_shows
+LEFT JOIN 
+    tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE 
+    tv_show_genres.genre_id IS NULL
+ORDER BY 
+    tv_shows.title ASC;
