0778370452d4e5a52afa82764df63eb13b46838a
nhonoree <honoreniyirema@gmail.com>
2024-11-21 14:33:51 -0800

    updates

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,9 +1,15 @@
--- Title: Genre ID by Show
--- Script to list all shows with at least one genre linked from `hbtn_0d_tvshows`
--- Each record displays: tv_shows.title - tv_show_genres.genre_id
--- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- List each TV show's title with its associated genre IDs
+-- If a show has multiple genres, list them on separate rows
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT 
+    tv_shows.title,          -- Title of the TV show
+    tv_show_genres.genre_id  -- ID of the genre linked to the show
+FROM 
+    tv_shows
+JOIN 
+    tv_show_genres 
+ON 
+    tv_shows.id = tv_show_genres.show_id  -- Linking TV shows with the linking table
+ORDER BY 
+    tv_shows.title ASC,      -- Sort results by show title
+    tv_show_genres.genre_id ASC;  -- Then sort by genre ID
