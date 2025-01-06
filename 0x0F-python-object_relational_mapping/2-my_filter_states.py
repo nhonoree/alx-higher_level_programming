ea03e34392d5c31decdfff9d840ac0c8484e2884
nhonoree <honoreniyirema@gmail.com>
2025-01-06 00:10:44 -0800

    updates

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,39 +1,34 @@
 #!/usr/bin/python3
-import sys
+"""
+Script that filters states by user input from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
-
-def main():
-    # Check if the script is executed with the correct number of arguments
-    if len(sys.argv) != 5:
-        print("Usage: ./2-my_filter_states.py <mysql_user> <mysql_password> <database_name> <state_name>")
-        return
-    
-    # Assign the arguments to variables
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL database
-    try:
-        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
-        cursor = db.cursor()
-
-        # Execute the query to filter states by name
-        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-        cursor.execute(query, (state_name,))
-        
-        # Fetch all rows and print them
-        states = cursor.fetchall()
-        for state in states:
-            print(state)
-
-        # Close the cursor and connection
-        cursor.close()
-        db.close()
-
-    except MySQLdb.Error as e:
-        print(f"Error: {e}")
+import sys
 
 if __name__ == "__main__":
-    main()
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Create a cursor to execute queries
+    cursor = db.cursor()
+
+    # Construct query with user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(sys.argv[4])
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Clean up
+    cursor.close()
+    db.close()
