aa5a0510b16574f2d275e024514fd2b00cb1fcbf
nhonoree <honoreniyirema@gmail.com>
2025-01-17 14:10:08 -0800

    updates

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
 """
-Search API script:
-Sends a POST request to a given URL with a letter as a parameter.
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
-
 import requests
 import sys
 
 if __name__ == "__main__":
-    # URL to send the POST request to
     url = "http://0.0.0.0:5000/search_user"
-    
-    # Retrieve the letter from command-line arguments or set q=""
     q = sys.argv[1] if len(sys.argv) > 1 else ""
-    payload = {'q': q}
-    
-    try:
-        # Send POST request
-        response = requests.post(url, data=payload)
-        response_json = response.json()
+    data = {"q": q}
 
-        # Check if the JSON response is not empty
-        if response_json:
-            print(f"[{response_json.get('id')}] {response_json.get('name')}")
+    try:
+        response = requests.post(url, data=data)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get("id"), json_data.get("name")))
         else:
             print("No result")
     except ValueError:
-        # Handle invalid JSON
         print("Not a valid JSON")
